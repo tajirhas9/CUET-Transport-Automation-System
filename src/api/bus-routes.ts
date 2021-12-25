00cb650451a7300cdd860e8819cc5eaf848909d8
Tajir Hasnain <u1604081@student.cuet.ac.bd>
2021-12-26 03:58:28 +0600

    api prototype

--- src/api/bus-routes.ts
+++
@@ -1,8 +0,0 @@
-import request from '@/utils/request'
-
-export const getRoutes = (params?: any) =>
-  request({
-    url: '/bus-routes',
-    method: 'get',
-    params
-  })
--- src/api/enums.ts
+++
@@ -1,7 +0,0 @@
-export enum Roles {
-    admin = 1,
-    staff = 2,
-    teacher = 3,
-    student = 4,
-    undefined = -1
-}
